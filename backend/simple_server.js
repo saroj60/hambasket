35aaa7f040c70f4742f95a430fb361582d7273f1
Your Name <you@example.com>
2026-01-05 11:20:35 +0545

    Cleanup: Prepare for Render deployment (Remove Docker/Debug files)

--- backend/simple_server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.send("Simple Server is working!"));
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Simple Server running on ${PORT}`);
-});
